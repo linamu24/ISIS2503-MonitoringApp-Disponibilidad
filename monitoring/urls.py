cc390ecd706934f4aa26f8ac34e7a1ea61972b18
Lina Muñoz <linamu24@gmail.com>
2025-04-30 22:23:59 -0500

    kong

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -21,7 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('health/', views.health_check, name='health'),
     path('', views.index),
-    path('', include('measurements.urls')),
-    path('', include('variables.urls')),
-    #path('', include('alarms.urls')),
+    path('', include('alarms.urls')),
 ]
